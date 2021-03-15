acaccb68f1f6a70e59655cded9d32486b1310f57
smedigo <smedigo@gmail.com>
2021-03-15 14:35:19 -0700

    controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,44 @@
 const db = require("../models");
 
 module.exports = {
-// getUserByID
-findAll: function(req, res) {
+  // getAllUsers
+
+  findAll: function (req, res) {
     db.User.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+
+  // getUserByID
+  findById: function (req, res) {
+    db.User.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+   //create user
+  create: function (req, res) {
+    db.User.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  // updateUser
+  update: function (req, res) {
+    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  // deleteUser
+  remove: function (req, res) {
+    db.User.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
-// getAllUsers
-// createUser
-// updateUser
-// deleteUser
 };
+
+
+
