6bce615cc688809c354a9034368ad47520d1e2c0
Sally Perez <seattlesally1@gmail.com>
2021-03-15 20:51:13 -0700

    added user api routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,36 @@
 const db = require("../models");
 
 module.exports = {
-// getUserByID
-findAll: function(req, res) {
+  // get all users
+  findAll: function (req, res) {
     db.User.find(req.query)
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
-// getAllUsers
-// createUser
-// updateUser
-// deleteUser
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // get user by id
+  findById: function (req, res) {
+    db.User.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // createUser
+  create: function (req, res) {
+    db.User.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // updateUser
+  update: function (req, res) {
+    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // deleteUser
+  remove: function (req, res) {
+    db.User.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,14 +4,27 @@ const userController = require("../../controllers/userController");
 // Matches with "/api/user"
 router
   .route("/")
-  .get(userController.findAll);
-  // .post(userController.create);
+  .get(userController.findAll)
+  .post(userController.create);
+
+  // router.post("/", async( req, res) => {
+  //   const user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //     neighborhood: req.body.neighborhood
+
+  //   })
+  //   await user.save();
+  //   res.send(user);
+  // })
 
 // Matches with "/api/user/:id"
-// router
-//   .route("/:id")
-//   .get(userController.findById)
-//   .put(userController.update)
-//   .delete(userController.remove);
+router
+  .route("/:id")
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.remove);
 
 module.exports = router;
+
