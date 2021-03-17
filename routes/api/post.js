232290af8aa268bd14def53703b3f5e0ce50ee92
smedigo <smedigo@gmail.com>
2021-03-17 14:23:41 -0700

    post.js routs

--- routes/api/post.js
+++ routes/api/post.js
@@ -4,19 +4,35 @@ const postController = require("../../controllers/postController");
 // Matches with "/api/user"
 
 router
-  .route("/")
-  .get(postController.findAll)
-  .post(postController.create);
+    .route("/")
+    .get(postController.findAll)
+    .post(postController.create);
 
-// Matches with "/api/posts"
+// Matches with "/api/posts"    //check this?
+router.post("/api/post", ({ body }, res) => {
+    db.Post.create({
 
+        name: body.name,
+        email: body.email,
+        password: body.password,
+        neighborhood: body.neighborhood
+
+    })
+        .then((dbPost) => {
+            res.json(dbPost);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(401).json(err);
+        });
+});
 
 // Matches with "/api/posts/:id"
 router
-  .route("/:id")
-  .get(postController.findById)
-  .put(postController.update)
-  .delete(postController.remove);
+    .route("/:id")
+    .get(postController.findById)
+    .put(postController.update)
+    .delete(postController.remove);
 
 
 
