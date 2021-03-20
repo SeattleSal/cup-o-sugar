ebb7cf814b5ef5eff8e85b85f9ee18dc391e3b92
Sally Perez <seattlesally1@gmail.com>
2021-03-20 13:11:34 -0700

    fix heroku

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use("/static", express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // // authentication route
