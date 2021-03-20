fea2637b6845014d43b56fa971657ebb5bb9e54c
smedigo <smedigo@gmail.com>
2021-03-20 13:37:54 -0700

    created dummy data in utils

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -14,9 +14,9 @@ class API {
      * @param {String} name 
      * @param {String} value 
      */
-    setHeader( name, value ) {
+    setHeader(name, value) {
 
-        if( value )
+        if (value)
 
             this.axios.defaults.headers.common[name] = value;
 
@@ -35,7 +35,7 @@ class API {
      * 
      * @returns {Promise}
      */
-    register( userData ) {
+    register(userData) {
         console.log(userData)
 
         return this.axios.post("/api/authentication/register", userData);
@@ -50,7 +50,7 @@ class API {
      * 
      * @returns {Promise}
      */
-    login( userData ) {
+    login(userData) {
 
         return this.axios.post("/api/authentication/login", userData);
 
@@ -61,6 +61,218 @@ class API {
         return this.axios.post("/api/authentication/authenticated");
 
     }
+
+    // end of auth API calls 
+
+    // getAllPost dummy data 
+
+    getAllPost() {
+
+        const postData = [
+            {
+                name: "Tricycle",
+                type: "give",
+                status: "open",
+                decription: "One year old, almost new",
+                picture: " https://cdn.pixabay.com/photo/2015/03/26/10/34/tricycle-691587_960_720.jpg",
+                owner: "Talor Swift",
+                responses: [
+                    {
+                        owner: " 604d24a2eb7588d074b6ad67 ",   //Taylor Swift ID
+                        chosen: true,
+                    }
+                ]
+
+
+            },
+
+            {
+                name: "Sofa",
+                type: "give",
+                status: "open",
+                decription: "Four years old, it has couple of stains on the side",
+                picture: " https://cdn.pixabay.com/photo/2017/08/06/15/44/house-2593570_960_720.jpg",
+                owner: "Chaka Khan",
+                responses: [
+                    {
+                        owner: " 604d24a2eb7588d074b6ad68",   // Chaka Kahn Id
+                        chosen: true,
+                    }
+                ]
+            },
+
+            {
+                name: "Plant",
+                type: "give",
+                status: "open",
+                decription: "Cactus",
+                picture: " https://cdn.pixabay.com/photo/2019/04/27/21/56/cactus-4161380_960_720.jpg",
+                owner: "Mariah Cary",
+                responses: [
+                    {
+                        owner: " 604d24a2eb7588d074b6ad69",    // Mariah Cary Id
+                        chosen: true,
+                    }
+                ]
+
+            },
+
+            {
+                name: "Scarf",
+                type: "give",
+                status: "open",
+                decription: "Homemade from alpaca yarn",
+                picture: " https://cdn.pixabay.com/photo/2017/08/06/14/42/blanket-2593141_960_720.jpg",
+                owner: "Shade",
+                responses: [
+                    {
+                        owner: "604d24a2eb7588d074b6ad65 ",    //  Shade Id
+                        chosen: true,
+                    }
+
+                ]
+            },
+        ];
+        return postData;
+
+
+    }
+
+    //getPostByUser
+
+    getPostByUser() {
+
+        const postDataByUser = [
+            {
+                name: "Tricycle",
+                type: "give",
+                status: "open",
+                decription: "One year old, almost new",
+                picture: " https://cdn.pixabay.com/photo/2017/08/06/15/44/house-2593570_960_720.jpg",
+                owner: "Talor Swift",
+                responses: [
+                    {
+                        owner: " Shade ",
+                        chosen: true,
+                    },
+                    {
+                        owner: " Mariah Cary ",
+                        chosen: false,
+                    }
+                ]
+
+
+            },
+
+            {
+                name: "Sofa",
+                type: "give",
+                status: "open",
+                decription: "Four years old, it has couple of stains on the side",
+                picture: "https://cdn.pixabay.com/photo/2017/08/06/15/44/house-2593570_960_720.jpg",
+                owner: "Talor Swift",
+                responses: [
+                    {
+                        owner: "Mariah Cary ",
+                        chosen: true,
+                    },
+                    {
+                        owner: "Chaka Kahn ",
+                        chosen: false,
+                    }
+                ]
+            },
+
+            {
+                name: "Plant",
+                type: "give",
+                status: "open",
+                decription: "Cactus",
+                picture: " https://cdn.pixabay.com/photo/2019/04/27/21/56/cactus-4161380_960_720.jpg",
+                owner: "Talor Swift",
+                responses: [
+                    {
+                        owner: " Chaka Kahn",
+                        chosen: true,
+                    },
+                    {
+                        owner: " Mariah Cary",
+                        chosen: false,
+                    }
+                ]
+
+            },
+
+            {
+                name: "Scarf",
+                type: "give",
+                status: "open",
+                decription: "Homemade from alpaca yarn",
+                picture: " https://cdn.pixabay.com/photo/2017/08/06/14/42/blanket-2593141_960_720.jpg",
+                owner: "Talor Swift",
+                responses: [
+                    {
+                        owner: "Chaka Kahn",
+                        chosen: true,
+                    },
+                    {
+                        owner: "Shade ",
+                        chosen: false,
+                    }
+
+                ]
+            },
+        ];
+        return postDataByUser;
+
+    }
+
+    //create post-wait
+
+    //getResponse
+
+    // getResponse() {
+
+    //     const responseData = [
+    //         {
+    //             owner: "Talor Swift",
+    //             owner: "",
+    //             chosen: true,
+
+    //         },
+
+    //         {
+    //             owner: "Chaka Khan",
+    //             owner: "",
+    //             chosen: false,
+
+
+    //         },
+
+    //         {
+    //             owner: "Mariah Cary",
+    //             owner: "",
+    //             chosen: true,
+
+
+    //         },
+
+    //         {
+
+    //             owner: "Shade",
+    //             owner: "",
+    //             chosen: false,
+
+    //         },
+    //     ]
+    //     return responseData;
+
+    // }
+
+
+    //delete a post-wait
+
+
 
 }
 
