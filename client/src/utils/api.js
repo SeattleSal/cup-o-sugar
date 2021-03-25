ac4c4479955fbc38aadb936b376b2823e0e02cdf
smedigo <smedigo@gmail.com>
2021-03-24 20:27:37 -0700

    mereged changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -71,175 +71,18 @@ class API {
 
     getAllPost() {
 
-<<<<<<< HEAD
         // Gets all posts  
         return axios.get("/api/post");
-=======
-        const postData = [
-            {
-                name: "Tricycle",
-                type: "give",
-                status: "open",
-                description: "One year old, almost new",
-                picture: " https://cdn.pixabay.com/photo/2015/03/26/10/34/tricycle-691587_960_720.jpg",
-                owner: "Talor Swift",
-                responses: [
-                    {
-                        owner: " 604d24a2eb7588d074b6ad67 ",   //Taylor Swift ID
-                        chosen: true,
-                    }
-                ]
-
-
-            },
-
-            {
-                name: "Sofa",
-                type: "give",
-                status: "open",
-                description: "Four years old, it has couple of stains on the side",
-                picture: " https://cdn.pixabay.com/photo/2017/08/06/15/44/house-2593570_960_720.jpg",
-                owner: "Chaka Khan",
-                responses: [
-                    {
-                        owner: " 604d24a2eb7588d074b6ad68",   // Chaka Kahn Id
-                        chosen: true,
-                    }
-                ]
-            },
-
-            {
-                name: "Plant",
-                type: "give",
-                status: "open",
-                description: "Cactus",
-                picture: " https://cdn.pixabay.com/photo/2019/04/27/21/56/cactus-4161380_960_720.jpg",
-                owner: "Mariah Cary",
-                responses: [
-                    {
-                        owner: " 604d24a2eb7588d074b6ad69",    // Mariah Cary Id
-                        chosen: true,
-                    }
-                ]
-
-            },
-
-            {
-                name: "Scarf",
-                type: "give",
-                status: "open",
-                description: "Homemade from alpaca yarn",
-                picture: " https://cdn.pixabay.com/photo/2017/08/06/14/42/blanket-2593141_960_720.jpg",
-                owner: "Shade",
-                responses: [
-                    {
-                        owner: "604d24a2eb7588d074b6ad65 ",    //  Shade Id
-                        chosen: true,
-                    }
-
-                ]
-            },
-        ];
-        return postData;
-
->>>>>>> 653e19a41056403a361364e0434ae1fe8b36da63
 
     }
 
     //getPostByUser
 
-<<<<<<< HEAD
     getPostByUser(id) {
 
        
         // Gets the post with the given id
         return axios.get("/api/post/" + id);
-=======
-    getPostByUser() {
-
-        const postDataByUser = [
-            {
-                name: "Tricycle",
-                type: "give",
-                status: "open",
-                description: "One year old, almost new",
-                picture: " https://cdn.pixabay.com/photo/2017/08/06/15/44/house-2593570_960_720.jpg",
-                owner: "Talor Swift",
-                responses: [
-                    {
-                        owner: " Shade ",
-                        chosen: true,
-                    },
-                    {
-                        owner: " Mariah Cary ",
-                        chosen: false,
-                    }
-                ]
-
-
-            },
-
-            {
-                name: "Sofa",
-                type: "give",
-                status: "open",
-                description: "Four years old, it has couple of stains on the side",
-                picture: "https://cdn.pixabay.com/photo/2017/08/06/15/44/house-2593570_960_720.jpg",
-                owner: "Talor Swift",
-                responses: [
-                    {
-                        owner: "Mariah Cary ",
-                        chosen: true,
-                    },
-                    {
-                        owner: "Chaka Kahn ",
-                        chosen: false,
-                    }
-                ]
-            },
-
-            {
-                name: "Plant",
-                type: "give",
-                status: "open",
-                description: "Cactus",
-                picture: " https://cdn.pixabay.com/photo/2019/04/27/21/56/cactus-4161380_960_720.jpg",
-                owner: "Talor Swift",
-                responses: [
-                    {
-                        owner: " Chaka Kahn",
-                        chosen: true,
-                    },
-                    {
-                        owner: " Mariah Cary",
-                        chosen: false,
-                    }
-                ]
-
-            },
-
-            {
-                name: "Scarf",
-                type: "give",
-                status: "open",
-                description: "Homemade from alpaca yarn",
-                picture: " https://cdn.pixabay.com/photo/2017/08/06/14/42/blanket-2593141_960_720.jpg",
-                owner: "Talor Swift",
-                responses: [
-                    {
-                        owner: "Chaka Kahn",
-                        chosen: true,
-                    },
-                    {
-                        owner: "Shade ",
-                        chosen: false,
-                    }
-
-                ]
-            },
-        ];
-        return postDataByUser;
->>>>>>> 653e19a41056403a361364e0434ae1fe8b36da63
 
     }
 
