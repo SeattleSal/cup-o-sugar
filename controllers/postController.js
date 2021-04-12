8447ed987f06fd57a44086a33ccb54da9e2a6291
Sally Perez <seattlesally1@gmail.com>
2021-04-12 13:09:58 -0700

    added comments on next steps for postcontroller

--- controllers/postController.js
+++ controllers/postController.js
@@ -63,15 +63,18 @@ module.exports = {
     update: function (req, res) {
         // console.log(req.user._id)
         console.log(req.body)
-        // create my 
         // responseOwner: req.user._id
         // status: "claimed"
         // db.Post.findOne({_id: req.params.id}).populate("User").execPopulate().then(data => res.json(data));
+        // to do - if post is already "claimed", send back error????
         db.Post.findOneAndUpdate({ _id: req.params.id }, {...req.body, responseOwner: req.user._id}, { new: true})
             .populate("User")
             .then(dbModel => {
                 db.User.findOne({_id: dbModel._doc.owner}).then(user => { 
+                    // check if claimed or not
+                    // if not claimed, use this line:
                     res.json({...dbModel._doc, owner: user});
+                    // else if claimed, send back rejection json?
 
                 })
 
